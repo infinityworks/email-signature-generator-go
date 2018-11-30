05beaf7375512e4fcffb887b091c1595d21d4588
Pete Craven <petecraven@Petes-MacBook-Pro.local>
2018-11-30 13:48:09 +0000

    Initial commit. Written in Go with a basic frontend that generates the email signature

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple Email Generator
+
+Simple app to generate an Infinity Works email signature. 
+Tested on Gmail
+
+### Running
+
+The app is built using Go
+
+It is already built so you should be able to run the applcation using
+
+```
+email-signature-generator-go
+```
+
+This spins up a Go server which hosts a form on 
+```
+localhost:8080
+```
+Enter your details and it will generate your email signature. All you need to do is copy it and paste into Gmail
---
+++ forms.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+)
+
+type SignatureDetails struct {
+	Name			string
+	JobTitle		string
+	OfficeNumber	string
+	MobileNumber	string
+	Email   		string
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("forms.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := SignatureDetails{
+			Name:			strings.ToUpper(r.FormValue("Name")),
+			JobTitle:		strings.ToUpper(r.FormValue("JobTitle")),
+			OfficeNumber:	r.FormValue("OfficeNumber"),
+			MobileNumber:	r.FormValue("MobileNumber"),
+			Email:			r.FormValue("Email"),
+		}
+
+		if details.Name == "" || details.JobTitle == "" || details.Email == "" {
+			tmpl.Execute(w, struct { 
+				Success 	bool
+				Error 	bool
+				Message	string
+			}{false, true, "Must have Name, Job Title, and Email provided"})
+			return
+		}
+		
+		fmt.Printf("Name => %s\n", details.Name)
+		fmt.Printf("Job Title => %s\n", details.JobTitle)
+		fmt.Printf("Office Number => %s\n", details.OfficeNumber)
+		fmt.Printf("Mobile Number => %s\n", details.MobileNumber)
+		fmt.Printf("EMAIL => %s\n", details.Email)
+
+		data := struct { 
+			Success 	bool
+			Signature 	SignatureDetails
+		}{true, details}
+
+		tmpl.Execute(w, data)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
